47fc781593d09122babbe81440c2e86bb4b88f52
admin <2031650588@qq.com>
2022-11-23 20:01:21 +0800

    ToolBar is ok.

--- src/gui/panel/MainPanel.java
+++ src/gui/panel/MainPanel.java
@@ -5,6 +5,7 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
 
+import gui.gui.listener.ToolBarListener;
 import util.CenterPanel;
 import util.GUIUtil;
 public class MainPanel extends JPanel {
@@ -50,6 +51,20 @@ public class MainPanel extends JPanel {
         setLayout(new BorderLayout());
         add(tb, BorderLayout.NORTH);
         add(workingPanel, BorderLayout.CENTER);
+
+        addListener();
+    }
+    private void addListener() {
+        ToolBarListener listener = new ToolBarListener();
+
+        bSpend.addActionListener(listener);
+        bRecord.addActionListener(listener);
+        bCategory.addActionListener(listener);
+        bReport.addActionListener(listener);
+        bConfig.addActionListener(listener);
+        bBackup.addActionListener(listener);
+        bRecover.addActionListener(listener);
+
     }
 
     public static void main(String[] args) {
---
+++ src/startup/Bootstrap.java
@@ -0,0 +1,22 @@
+package startup;
+
+import gui.frame.MainFrame;
+import gui.panel.MainPanel;
+import gui.panel.SpendPanel;
+import util.GUIUtil;
+
+import javax.swing.*;
+import java.lang.reflect.InvocationTargetException;
+
+public class Bootstrap {
+    public static void main(String[] args) throws InvocationTargetException, InterruptedException {
+        GUIUtil.useLNF();
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                MainFrame.instance.setVisible(true);
+                MainPanel.instance.workingPanel.show(SpendPanel.instance);
+            }
+        });
+    }
+}
